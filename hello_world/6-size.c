dc291bb3c29a77d584783134fe3990a84fc28ddd
gr3nvaltblack <dlk9023x@gmail.com>
2024-10-22 11:10:57 +0000

    prints the size of various types

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,1 +1,18 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+*main - is the entry point of a C program.
+*It's where the program starts executing.
+*When you run a C program, the operating system calls the main function first.
+*Everything inside the main function is executed step by step.
+*It's like the starting line of a race for your code
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("size of char in bytes: %lu\n" sizeof(char));
+printf("size of int in bytes: %lu\n" sizeof(int));
+printf("size of long int in bytes: %lu\n" sizeof(long int));
+printf("size of float in bytes: %lu\n" sizeof(float));
+return (0);
+}
