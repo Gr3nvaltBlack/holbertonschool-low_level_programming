8b4410e39ce0275a586d558240b0844691b2f386
gr3nvaltblack <dlk9023x@gmail.com>
2024-10-22 11:31:41 +0000

    gfhgdg

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
-*main - is the entry point of a C program.
-*It's where the program starts executing.
-*When you run a C program, the operating system calls the main function first.
-*Everything inside the main function is executed step by step.
-*It's like the starting line of a race for your code
-*Return: Always 0 (Success)
+* main - is the entry point of a C program.
+* It's where the program starts executing.
+* When you run a C program, the operating system calls the main function first.
+* Everything inside the main function is executed step by step.
+* It's like the starting line of a race for your code
+* Return: Always 0 (Success)
 */
 int main(void)
 {
