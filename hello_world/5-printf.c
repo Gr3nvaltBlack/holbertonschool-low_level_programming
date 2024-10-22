7a65c471cb0102bd67940af6e312d57a505021b0
gr3nvaltblack <dlk9023x@gmail.com>
2024-10-22 09:06:22 +0000

    C program printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,1 +1,14 @@
-#!/bin/bash
+#include<stdio.h>
+
+
+/**
+*main - This uses program prints exactly the string
+*Programming is like building a multilingual puzzle
+*followed by a new line.
+*Return: always 0 (Success)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
