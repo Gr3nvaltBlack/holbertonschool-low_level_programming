f476379b37ab3edb3592d475b34e2dd20db9a214
gr3nvaltblack <dlk9023x@gmail.com>
2024-10-22 14:31:16 +0000

    prints the size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 
 /**
-* main - is the entry point of a C program.
-* It's where the program starts executing.
-* When you run a C program, the operating system calls the main function first.
-* Everything inside the main function is executed step by step.
-* It's like the starting line of a race for your code
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-printf("size of char in byte(s): %lu\n", sizeof(char));
-printf("size of int in byte(s): %lu\n", sizeof(int));
-printf("size of long int in byte(s): %lu\n", sizeof(long int));
-printf("size of long long int in byte(s): %lu\n", sizeof(long long int));
-printf("size of float in byte(s): %lu\n", sizeof(float));
-return (0);
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+        printf("Size of an int: %zu byte(s)\n", sizeof(int));
+        printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+        printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+        printf("Size of a float: %zu byte(s)\n", sizeof(float));
+        return (0);
 }
