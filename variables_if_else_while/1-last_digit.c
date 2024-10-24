70547c281d785c76db9a50acbd40a91e2314e1e9
gr3nvaltblack <dlk9023x@gmail.com>
2024-10-24 15:50:35 +0000

    a random number to the variable

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ if (n > 5)
 printf("%d and is greater than 5\n", n);
 else if (n == 0)
 printf("%d and is 0\n", n);
-if (n > 6 && n != 0)
+else
 printf("%d and is less than 6 and not 0\n", n);
 
 return (0);
