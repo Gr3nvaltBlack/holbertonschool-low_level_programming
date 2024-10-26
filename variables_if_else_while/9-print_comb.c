5c41c80cb4242874149a02e891a352bb6684a887
gr3nvaltblack <dlk9023x@gmail.com>
2024-10-26 17:13:44 +0000

    prints all possible combinations

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - Entry Point
+ * main - Entry point
  *
- * Return: Always 0 (Sucess)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 int n = 0;
-while (n <= '9')
+while (n <= 9)
 {
 putchar(n + '0');
-n++;
 if (n < 9)
 {
 putchar(',');
 putchar(' ');
 }
+n++;
 }
 putchar('\n');
 return (0);
