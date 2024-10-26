99057e05c473fee8a8f5acbe3a4d66be261354eb
gr3nvaltblack <dlk9023x@gmail.com>
2024-10-26 09:50:07 +0000

    prints all the numbers of base 16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - Entry Point
+*
+*Return: Always 0 (Sucess)
+*/
+int main(void)
+{
+int n = '0';
+char lettre = 'a';
+while (n <= 9)
+{
+putchar(n + '0');
+n++;
+}
+while (lettre <= 'f')
+{
+putchar(lettre);
+lettre++;
+}
+putchar('\n');
+return (0);
+}
