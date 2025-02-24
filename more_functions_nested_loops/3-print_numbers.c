98f2dcda3ce5fbb30c479bfffc35f3b2054f6720
Gr3nvaltBlack <dlk9023x@gmail.com>
2025-02-24 15:36:43 +0000

    prints the numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints the number
+ *
+ *
+ */
+void print_numbers(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -7,7 +7,7 @@
 
 
 
-
+void print_numbers(void);
 int mul(int a, int b);
 int _isdigit(int c);
 int _isupper(int c);
