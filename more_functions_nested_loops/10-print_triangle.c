49a3441b1898528951b04306242d846bb13aae5d
Gr3nvaltBlack <dlk9023x@gmail.com>
2025-02-25 15:00:10 +0000

    prints a triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -11,13 +11,13 @@ void print_triangle(int size)
 
 	if (size > 0)
 	{
-		for (i = 0; i < size; i++)
+		for (i = 1; i <= size; i++)
 		{
-			for (j = size; j > i + 1; j--)
+			for (j = size - i; j > 0; j--)
 			{
-				_putchar(35);
+				_putchar(32);
 			}
-			for (k = 0; k < i + 1; k++)
+			for (k = 1; k <= i; k++)
 			{
 				_putchar(35);
 			}
