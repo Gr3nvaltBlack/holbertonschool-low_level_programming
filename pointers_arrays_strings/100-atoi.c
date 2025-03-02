102a1b6857ed6595e24ab8deee5a68e7df666c1c
Gr3nvaltBlack <dlk9023x@gmail.com>
2025-03-02 16:03:32 +0000

    convert a string to an integer

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,40 +1,40 @@
 #include "main.h"
 
 /**
- * _atoi - function that convert a string to an integer
- *@s: pointer that displays the numbers in the string
- *Return: nomber * sign
+ * _atoi - Convert a string to an integer
+ * @s: The string to convert
+ *
+ * Return: The integer value of the string, or 0 if no numbers found
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, nb = 0, found_number = 0;
+	int result = 0;
+	int sign = 1;
+	int started = 0;
 
-	while (s[i] != '\0')
+	while (*s)
 	{
-		if (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13))
+		if (*s == '-')
 		{
-			i++;
-			continue;
-		}
-		else if (s[i] == '-' || s[i] == '+')
-		{
-			if (found_number)
-				break;
-			if (s[i] == '-')
+			if (!started)
 				sign *= -1;
 		}
-		else if (s[i] >= '0' && s[i] <= '9')
+		else if (*s == '+')
 		{
-			nb = (nb * 10) + (s[i] - '0');
-			found_number = 1;
-		}
-		else
-		{
-			if (found_number)
+			if (started)
 				break;
 		}
-		i++;
+		else if (*s >= '0' && *s <= '9')
+		{
+			started = 1;
+			result = result * 10 + (*s - '0') * sign;
+		}
+		else if (started)
+		{
+			break;
+		}
+		s++;
 	}
 
-	return (nb * sign);
+	return (result);
 }
