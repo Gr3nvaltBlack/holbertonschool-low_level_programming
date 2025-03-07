2ccf855555dc574a51a6c43a9a3679decd61f321
Gr3nvaltBlack <dlk9023x@gmail.com>
2025-03-07 22:50:14 +0000

    prints its name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - display the name program
+ * @argc: number of arrays
+ * @argv: arguments arrays
+ *
+ * Return: Toujours 0 (Succès)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
