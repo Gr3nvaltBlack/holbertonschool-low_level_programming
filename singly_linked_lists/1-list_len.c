629100d8dd0e0bc58016ff6c1c14d1ef75523ae8
Gr3nvaltBlack <dlk9023x@gmail.com>
2025-04-05 19:19:04 +0000

    return the number of elements in a linked list

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * list_len - counts the number of elements in a linked list_t list
+ * @h: list_t list
+ * Return: number of elements (index)
+ */
+size_t list_len(const list_t *h)
+{
+	size_t compt = 0;
+
+	while(h != NULL)
+	{
+		compt++;
+		h = h-> next;
+	}
+	return (compt);
+}
